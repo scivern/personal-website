cf5eb6c34e1a65b0f135152ca8a127cf999966a6
scivern <georgescannell@outlook.com>
2024-02-11 23:55:53 +0000

    basic graph of artists displayed

--- script.js
+++ script.js
@@ -6,6 +6,78 @@ function myFunction(imgs) {
     expandImg.parentElement.style.display = "block";
 }
 
+function dictionarySorter(dictionary) {
+
+    var keyValues = []
+
+    for (var key in dictionary) {
+        keyValues.push([key, dictionary[key]])
+    }
+    keyValues.sort(function compare(kv1, kv2) {
+        // This comparison function has 3 return cases:
+        // - Negative number: kv1 should be placed BEFORE kv2
+        // - Positive number: kv1 should be placed AFTER kv2
+        // - Zero: they are equal, any order is ok between these 2 items
+        return kv2[1] - kv1[1]
+    })
+    return keyValues;
+}
+
+function dataCleaner(data) {
+
+    let uniqueArtists = {};
+    let uniqueSongs = {};
+    let uniqueYears = {};
+    let uniqueMonths = {};
+    let uniqueTimes = {};
+
+    for (let stream in data) {
+
+        if (data[stream]["ms_played"] < 60000 || data[stream]['master_metadata_album_artist_name'] === null) {
+            continue;
+        }
+
+        if (data[stream]['ts'].slice(0, 4) in uniqueYears) {
+            uniqueYears[data[stream]['ts'].slice(0, 4)] += 1;
+        } else {
+            uniqueYears[data[stream]['ts'].slice(0, 4)] = 1;
+        }
+
+        if (data[stream]['ts'].slice(5, 7) in uniqueMonths) {
+            uniqueMonths[data[stream]['ts'].slice(5, 7)] += 1;
+        } else {
+            uniqueMonths[data[stream]['ts'].slice(5, 7)] = 1;
+        }
+
+        if (data[stream]['ts'].slice(11, 13) in uniqueTimes) {
+            uniqueTimes[data[stream]['ts'].slice(11, 13)] += 1;
+        } else {
+            uniqueTimes[data[stream]['ts'].slice(11, 13)] = 1;
+        }
+
+        if (data[stream]['master_metadata_album_artist_name'] in uniqueArtists) {
+            uniqueArtists[data[stream]['master_metadata_album_artist_name']] += 1;
+        } else {
+            uniqueArtists[data[stream]['master_metadata_album_artist_name']] = 1;
+        }
+
+        if (data[stream]['master_metadata_track_name'] in uniqueSongs) {
+            uniqueSongs[data[stream]['master_metadata_track_name']] += 1;
+        } else {
+            uniqueSongs[data[stream]['master_metadata_track_name']] = 1;
+        }
+
+    }
+    
+    // uniqueYears = dictionarySorter(uniqueYears);
+    // uniqueMonths = dictionarySorter(uniqueMonths);
+    // uniqueTimes = dictionarySorter(uniqueTimes);
+    uniqueArtists = dictionarySorter(uniqueArtists);
+    uniqueSongs = dictionarySorter(uniqueSongs);
+
+    return [uniqueYears, uniqueMonths, uniqueTimes, uniqueArtists, uniqueSongs];
+}
+
 function readFileAsText(file) {
     return new Promise(function (resolve, reject) {
         let fr = new FileReader();
@@ -43,31 +115,52 @@ function combineFiles(ev) {
         for (let i = 0; i < values.length; i++) {
             combinedJson = combinedJson.concat(values[i])
         }
-        // console.log(typeof combinedJson[0]["ts"])
-        // console.log(combinedJson[0]["ts"]);
-        dataCleaner(combinedJson);
+
+        let cleanedData = dataCleaner(combinedJson);
+        const arrayColumn = (arr, n) => arr.map(x => x[n]);
+        const dataKey = arrayColumn(cleanedData[3].slice(10, 20),0);
+        const dataVal = arrayColumn(cleanedData[3].slice(10, 20), 1);
+        // console.log(dataKey,dataVal,cleanedData[3])
+        // let dataa = {"11": 1, "12": 3, "1": 2}; 
+        let myChart = document.getElementById("myChart").getContext("2d");
+
+        let chart = new Chart(myChart, {
+            type: "bar",
+            data: {
+                labels: dataKey,
+                datasets: [{
+                    // label: "bruh",
+                    data: dataVal//(cleanedData[3].slice(50,55))[1]
+                }],
+            },
+            options: {}
+        });
+
+
 
     });
 }
 
 
-function dataCleaner(data) {
+
+
+/*function dataCleaner(data) {
 
     let uniqueArtists = {};
     let uniqueSongs = {};
     let uniqueYears = {};
     let uniqueMonths = {};
     let uniqueTimes = {};
-    
+
     for (let stream in data) {
-    
+
         if (data[stream]["ms_played"] < 60000 || data[stream]['master_metadata_album_artist_name'] === null) {
             continue;
         }
-   
+
         if (data[stream]['ts'].slice(0, 4) in uniqueYears) {
             uniqueYears[data[stream]['ts'].slice(0, 4)] += 1;
-        }   else {
+        } else {
             uniqueYears[data[stream]['ts'].slice(0, 4)] = 1;
         }
 
@@ -95,26 +188,13 @@ function dataCleaner(data) {
             uniqueSongs[data[stream]['master_metadata_track_name']] = 1;
         }
 
-
-
-
-
     }
-    console.log(uniqueSongs);
-    // console.log(uniqueYears,uniqueMonths,uniqueTimes);
-    console.log(data)
-}
 
-/*let myChart = document.getElementById("myChart").getContext("2d");
+    // uniqueYears = dictionarySorter(uniqueYears);
+    // uniqueMonths = dictionarySorter(uniqueMonths);
+    // uniqueTimes = dictionarySorter(uniqueTimes);
+    uniqueArtists = dictionarySorter(uniqueArtists);
+    uniqueSongs = dictionarySorter(uniqueSongs);
 
-let chart = new Chart(myChart, {
-    type: "bar",
-    data: {
-        labels: ["a", "b", "c"],
-        datasets: [{
-            label: 'bruh',
-            data: [1, 2, 3]
-        }],
-    },
-    options: {}
-});*/
+    return [uniqueYears, uniqueMonths, uniqueTimes, uniqueArtists, uniqueSongs];
+}*/
