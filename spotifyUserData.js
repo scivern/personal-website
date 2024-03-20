b1ca871d4e8a42cde3aee9535c7a5da19dccadea
scivern <georgescannell@outlook.com>
2024-03-20 21:55:54 +0000

    spotify user data mostly done. maybe some styling

--- spotifyUserData.js
+++ spotifyUserData.js
@@ -15,6 +15,8 @@ const scope = "user-read-private user-follow-read user-library-read user-read-em
 
 document.addEventListener("DOMContentLoaded", onPageLoad, false);
 document.querySelector(".login-button").addEventListener("click", () => requestAuthorization(), false)
+document.querySelector(".get-saved-tracks-button").addEventListener("click", () => getSavedTracks(), false)
+document.querySelector(".followed-concerts-button").addEventListener("click", () => getFollowedArtists(), false)
 
 function onPageLoad(){
 
@@ -35,7 +37,7 @@ function onPageLoad(){
             document.querySelector(".logout-button").addEventListener("click", () => logOut(), false)
    
             getUserData();
-            getFollowedArtists();
+            // getFollowedArtists();
         }
     }
 }
@@ -183,10 +185,18 @@ async function apiCall(url) {
 async function getUserData() {
 
     data = await apiCall("https://api.spotify.com/v1/me")
-
+    localStorage.setItem("userCountry", data.country)
     document.querySelector('.profile-pic').src = data.images[1].url
-    document.querySelector('.login-text').appendChild(document.createTextNode(data.display_name))
+    document.querySelector('.logged-in-text').appendChild(document.createTextNode(data.display_name))
 }
+
+async function ticketmasterApi(url) {
+    const call = await fetch(url, {
+        method: "GET",
+    })
+    return await call.json();
+}
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 async function getFollowedArtists() {
     async function repeatGet(url, followedTotal = []) {
@@ -197,12 +207,69 @@ async function getFollowedArtists() {
 
         if (followedObject.artists.next != undefined) { await repeatGet(followedObject.artists.next, followedTotal) }
         
-        return { "artists": followedTotal }
+        return followedTotal
     }
 
     followedTotal = await repeatGet("https://api.spotify.com/v1/me/following?type=artist&limit=50")
     console.log(followedTotal);
-    return followedTotal;
+
+    // let city = "London";
+    // let radius = 100;
+    let events = []
+    let followedArtistShows = {}
+    userCountry = localStorage.getItem("userCountry")
+    for (i = 0; i < 5; i++) {
+        response = await ticketmasterApi("https://app.ticketmaster.com/discovery/v2/events.json?&size=20&apikey=dYtFUjzPcwSQU8GRUj4hRBfDaLkOLxql& \
+        radius=0&countryCode=" + userCountry + "&keyword=" + followedTotal[i])//+ "&city=" + "d" + "&radius=" + radius
+        console.log(response);
+        await sleep(200);
+        if (response._embedded == undefined) {continue}
+
+        document.querySelector(".followed-concerts-accordions").innerHTML += "<button class='accordion' id=" + followedTotal[i] + "-accordion >" + followedTotal[i] + "</button>"
+
+        let table = document.createElement('table');
+        table.className = ("panel")
+        table.style = "border-spacing: 10px"
+        row = table.insertRow();
+        row.style = "color: rgb(221, 179, 42)"
+        row.insertCell(0).appendChild(document.createTextNode("Name"))
+        row.insertCell(1).appendChild(document.createTextNode("City"))
+        row.insertCell(2).appendChild(document.createTextNode("Date"))
+        
+        events = [];
+        for (event of response._embedded.events) {
+            events.push({ "Name": event.name, "Venue": event._embedded.venues[0].city.name, "Date": event.dates.start.localDate, "Url": event.url});
+            row = table.insertRow();
+            let a = document.createElement('a'); a.href = event.url; a.target = "_blank"; a.style = "color: white; text-decoration: none"
+            a.appendChild(document.createTextNode(event.name))
+            row.insertCell(0).appendChild(a)
+            row.insertCell(1).appendChild(document.createTextNode(event._embedded.venues[0].city.name))
+            row.insertCell(2).appendChild(document.createTextNode(event.dates.start.localDate))
+            
+            
+        }
+        followedArtistShows[followedTotal[i]] = events
+        document.querySelector(".followed-concerts-accordions").append(table)
+    }   
+    console.log(followedArtistShows);
+
+    var acc = document.querySelectorAll(".accordion");
+    // var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("accordion-active");
+            var panel = this.nextSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+    
+    
+    // return followedTotal;
 }
 
 async function getSavedTracks() {
@@ -228,7 +295,8 @@ async function getSavedTracks() {
     const link = document.createElement('a')
     link.setAttribute('href', objUrl)
     link.setAttribute('download', 'File.csv')
-    link.textContent = 'Click to Download'
+    // link.textContent = 'Click to Download'
 
-    document.querySelector('body').append(link)
+    document.querySelector('.download-tracks').append(link)
+    link.click()
 }
